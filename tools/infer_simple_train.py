aacfb2e67af60bc9900a5e9fabb263391b52e807
PacteraOliver <zhuoran.wu@pactera.com>
2018-06-06 14:35:38 -0700

    test: try confidence score with 0.8

--- tools/infer_simple_train.py
+++ tools/infer_simple_train.py
@@ -102,7 +102,7 @@ def main(args):
     dummy_wider_dataset = wider_datasets.get_wider_dataset()
 
     INFER_BOX_ALPHA = 0.3
-    INFER_THRESH = 0.9
+    INFER_THRESH = 0.8
     INFER_KP_THRESH = 2
     MODEL_ITER = 60000
 
