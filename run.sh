9180fcecfe7260e50906e99e80819d900630ba09
PacteraOliver <zhuoran.wu@pactera.com>
2018-06-06 12:26:35 -0700

    test: apply nms with 0.3

--- run.sh
+++ run.sh
@@ -9,3 +9,5 @@ wget https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e
 cd ..
 
 python2 tools/infer_simple_bbox.py --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml --output-dir /tmp/detectron-visualizations_bbox --image-ext jpg --wts weights/model_final.pkl data/train
+
+python2 tools/infer_simple_train.py --cfg configs/12_2017_baselines/wider_e2e_faster_rcnn_R-101-FPN_1x.yaml --output-dir /tmp/detectron-visualizations_bbox --image-ext jpg --wts weights/model_train_60000.pkl data/val
